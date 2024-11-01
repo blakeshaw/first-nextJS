6bc1ca86896bdaaf844933af1b5f69740d18b854
blakeshaw <blakeshaw5450@gmail.com>
2024-11-01 15:42:07 -0600

    Added counter.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Counter from '../components/Counter';
 
 export default function Home() {
   return (
@@ -8,11 +9,12 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <h1 className={styles.title}>
-        Gregory Shaw welcomes <a href="https://nextjs.org">Next.js!</a>
-      </h1>
 
+      <h1 className={styles.title}>
+          Gregory Shaw welcomes <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <Counter />
+        
     </div>
   );
 }
